ef7e54d334cb56b8bcfd0fcfc01f7fded33aaa63
gleft <gleft52@yahoo.gr>
2014-05-12 15:17:28 +0300

    registration almost done

--- reg.php
+++ reg.php
@@ -1,28 +1,20 @@
 <?php
-        include "config.php";
+        //include 'config.php';
         session_start();
-        $link=mysql_connect($db_server["host"], $db_server["username"],
-$db_server["password"]) or die(mysql_error());
-        mysql_select_db($db_server["database"]) or die(mysql_error());
-        mysql_query("SET CHARACTER SET greek", $link);
-
-        $allquery="select * from users";
-        $allresult=mysql_query($allquery) or die(mysql_error());
-
-        for ($j=0; $j<mysql_num_rows($allresult); $j++)
-        {
-                $allrow = mysql_fetch_row($allresult);
-                $alllastname=$allrow[1];
-                $allfrstname=$allrow[2];
-                $allemail=$allrow[4];
-
-                $endiamesquery="update nendiamesosapof set
-nenapofemail='".$allemail."' where (nenapoflastname='".$alllastname."' AND
-nenapoffrstname='".$allfrstname."')";
-
-                $endiamesresult=mysql_query($endiamesquery) or
-die(mysql_error());
-        }
-
-        mysql_close;
+        $link=mysqli_connect("localhost","root","","dimos") or die(mysql_error());
+	if (mysqli_connect_errno()) {
+ 	echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	}
+        //mysql_select_db($db_server["dimos"]) or die(mysql_error());
+        //mysql_query("SET CHARACTER SET greek", $link);
+	$email = $_POST['email'];
+	$password = $_POST['password'];
+	$name =  $_POST['name'];
+	$phone = $_POST['phone'];
+	$sql="INSERT INTO users (email, password, name, phone) VALUES ('$email', '$password', '$name', '$phone')";
+	if (!mysqli_query($link,$sql)) {
+  die('Error: ' . mysqli_error($link));
+}
+echo "1 record added";
+        mysqli_close($link);
 ?>
